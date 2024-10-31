520680c52791ab31703cd0593b1c25738e849c07
Mofdy <mofdyelia@gmail.com>
2024-10-31 10:55:47 +0300

    Guard user

---
+++ src/app/demo/guards/userguard.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { map, Observable, take, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class userguardGuard implements CanActivate {
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.afAuth.authState.pipe(
+      take(1),
+      map(user => !!user),
+      tap(loggedIn => {
+        if (!loggedIn) {
+          console.log('Access denied');
+          this.router.navigate(['/login']);
+        }
+      })
+    );
+  }
+}
