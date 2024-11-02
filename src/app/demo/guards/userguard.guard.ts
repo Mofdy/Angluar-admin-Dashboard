0f92945f87050158f608281f0e5b5cf3007a22c7
Mofdy <mofdyelia@gmail.com>
2024-11-02 12:03:43 +0200

    Merge

--- src/app/demo/guards/userguard.guard.ts
+++ src/app/demo/guards/userguard.guard.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-<<<<<<< HEAD
 import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-=======
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
-import { map, Observable, take, tap } from 'rxjs';
->>>>>>> 520680c52791ab31703cd0593b1c25738e849c07
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +10,6 @@ import { map, Observable, take, tap } from 'rxjs';
 export class userguardGuard implements CanActivate {
   constructor(private afAuth: AngularFireAuth, private router: Router) {}
 
-<<<<<<< HEAD
   canActivate(): Observable<boolean> {
     return this.afAuth.authState.pipe(
       map(user => {
@@ -24,19 +18,6 @@ export class userguardGuard implements CanActivate {
         } else {
           this.router.navigate(['/guest/login']);
           return false;
-=======
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean> | Promise<boolean> | boolean {
-    return this.afAuth.authState.pipe(
-      take(1),
-      map(user => !!user),
-      tap(loggedIn => {
-        if (!loggedIn) {
-          console.log('Access denied');
-          this.router.navigate(['/login']);
->>>>>>> 520680c52791ab31703cd0593b1c25738e849c07
         }
       })
     );
