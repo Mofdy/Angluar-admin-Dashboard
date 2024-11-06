b6a79ea36283f6cb1a5fcabffb9f09d45f20401e
mohamedfathy32 <mf876026@gmail.com>
2024-11-06 02:47:08 +0200

    total sales for taday , month , year and total

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -5,7 +5,6 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Title } from '@angular/platform-browser';
 import { Icategories } from '../models/icategories';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -18,33 +17,29 @@ export class FoodService {
   getCategories(): Observable<Icategories[]> {
     return this.firestore.collection<Icategories>('categories').valueChanges();
   }
+  
   // getFoodById(id: string): Observable<ifood | undefined> {
   //   return this.firestore.collection<ifood>('product',prd=>prd.where()).doc(id).valueChanges();
   // }
-  
+
   async addFood(food: ifood): Promise<void> {
     await this.firestore.collection('products').add(food);
   }
 
-  
-   updateFood( title:string ,food: ifood): Promise<void> {
+  updateFood(title: string, food: ifood): Promise<void> {
     return this.firestore.collection(`products`).doc(title).update(food);
   }
 
   deleteFood(title: string): Promise<void> {
     return this.firestore.collection('products').doc(title).delete();
   }
-  
-getDocumentByTitle(collection: string, titleEn: string): Observable<any> {
-  return this.firestore
-    .collection(collection, ref => ref.where('title.en', '==', titleEn))
-    .valueChanges({ idField: 'id' }) // جلب الـ ID مع الوثيقة
-    .pipe(
-      map(docs => (docs.length > 0 ? docs[0] : null)) // جلب أول وثيقة مطابقة
-    );
+
+  getDocumentByTitle(collection: string, titleEn: string): Observable<any> {
+    return this.firestore
+      .collection(collection, (ref) => ref.where('title.en', '==', titleEn))
+      .valueChanges({ idField: 'id' }) // جلب الـ ID مع الوثيقة
+      .pipe(
+        map((docs) => (docs.length > 0 ? docs[0] : null)) // جلب أول وثيقة مطابقة
+      );
+  }
 }
-
-}
-
-
-
